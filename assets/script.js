a1d5aebc0bf60190d673bf2d9a719b8683ba3fd2
derekthecarrot <jpastvait@gmail.com>
2021-08-28 10:03:17 -0400

    updating html, adding classlist in jscript to get start button functioning to make elements show and hide

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,33 @@
 
 // STARTS TIMER ON START QUIZ BUTTON
-$("#start-btn").click( function(){
-    var counter = 60;
-    setInterval(function() {
-      counter--;
-       if (counter >= 0) {
-          span = document.getElementById("count");
-          span.innerHTML = counter;
-       }
-       if (counter === 0) {
-          alert('sorry, out of time');
-          clearInterval(counter);
-        }
-      }, 1000);
- });
+const startButton = document.getElementById('start-btn')
+const sectionOne = document.getElementById('section-1')
+const sectionTwo = document.getElementById('section-2')
+const timerSection = document.getElementById('timer-section')
+
+startButton.addEventListener('click', startGame)
+
+var timeEl = document.getElementById("time");
+var secondsLeft = 60;
+
+function startGame(){
+
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+
+      if (secondsLeft === 0) {
+          // Stops execution of action at set interval
+          clearInterval(timerInterval);
+          alert = "Time is up!";
+
+      }
+      sectionOne.classList.add('hide')
+      sectionTwo.classList.remove('hide')
+      timerSection.classList.remove('hide')
+
+      
+      
+  }, 1000);
+}
