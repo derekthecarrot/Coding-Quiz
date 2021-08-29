d49ffe3b1507c3afbb840d4e8b134cfdc85e21c4
derekthecarrot <jpastvait@gmail.com>
2021-08-29 18:29:00 -0400

    updating highscore js code and adding to highscore.html

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,6 +1,10 @@
 //highscore input
 const username = document.getElementById("username")
 const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore')
+const mostRecentScore = localStorage.getItem("mostRecentScore")
+
+finalScore.innerText = mostRecentScore; // need to set a localstorage in index.html to get mostRecentScore
 
 username.addEventListener("keyup", () => {
     console.log(username.value);
